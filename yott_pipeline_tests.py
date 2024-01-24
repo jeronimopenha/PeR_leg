affa9e6d8ca483b7aa96883901fbb6e5b8090923
Fábio Trindade <fabiotrindadefer@gmail.com>
2024-01-24 18:57:11 -0300

    Fix(*): commented unnecessary prints

--- yott_pipeline_tests.py
+++ yott_pipeline_tests.py
@@ -3,7 +3,7 @@ from src.util.per_graph import PeRGraph
 from src.util.yott.yott import YOTT
 
 
-per_graph = PeRGraph("/home/fabio/Mestrado/PeR/dot_db/connected/arf.dot")
+per_graph = PeRGraph("/home/fabio/Mestrado/PeR/dot_db/connected/cosine2.dot")
 annotations = [[0,1],[0,2],[0,3],[0,2],[-1,-1],[-1,-1]]
 yott_pipeline = YOTTPipeline(annotations,per_graph,7)
 print(yott_pipeline.run(1))
