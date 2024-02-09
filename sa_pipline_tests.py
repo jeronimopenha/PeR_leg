6684c148c4963631797ec80e9a0a48ded5788548
Jeronimo <jeronimopenha@gmail.com>
2024-02-09 10:11:28 -0300

    tests

--- sa_pipline_tests.py
+++ sa_pipline_tests.py
@@ -1,4 +1,3 @@
-from multiprocessing import Pool
 import time
 from src.sw.sa_pipeline.sa_pipeline_sw import SAPipeline
 from src.util.per_graph import PeRGraph
@@ -8,7 +7,7 @@ from src.util.util import Util
 
 def run_connected_graphs():
     arch_types = [ArchType.ONE_HOP]
-    total_threads = [6]
+    total_threads = [60]
 
     threads_per_copy: int = 6
     make_shuffle: bool = True
@@ -24,32 +23,15 @@ def run_connected_graphs():
     # FIXME the line below is only for debugging
     dots_list = [[dot_connected_path + 'mac.dot', 'mac.dot']]
 
-    # jobs_alive = []
-    # manager = mp.Manager()
-    # return_dict = manager.dict()
-    task_counter = 0
-    varst = []
-    futures = []
-    outputs = []
-    pool = Pool()
+
     for th in total_threads:
         for arch_type in arch_types:
             local_counter = 0
             for dot_path, dot_name in dots_list:
                 per_graph = PeRGraph(dot_path, dot_name)
                 sa_pipeline = SAPipeline(per_graph, arch_type, distance_table_bits, make_shuffle, threads_per_copy)
-                print("task:", task_counter, ",", "local_task:", local_counter)
                 res = sa_pipeline.run(th // threads_per_copy, )
-                # res = pool.apply_async(sa_pipeline.run, (th // threads_per_copy,))
-                # res.wait()
-                # print(res.ready())
-                futures.append(res)
-                local_counter += 1
-    pool.close()
-    pool.join()
-
-    for output in futures:
-        print(output.get())
+                print(res)
 
 
 if __name__ == '__main__':
--- src/sw/sa_pipeline/sa_pipeline_sw.py
+++ src/sw/sa_pipeline/sa_pipeline_sw.py
@@ -13,7 +13,7 @@ from src.sw.sa_pipeline.stage8_sa import Stage8SA
 from src.sw.sa_pipeline.stage9_sa import Stage9SA
 from src.sw.sa_pipeline.stage10_sa import Stage10SA
 from src.util.util import Util
-from multiprocessing import Pool
+from concurrent.futures.process import ProcessPoolExecutor
 
 
 class SAPipeline(PiplineBase):
@@ -24,63 +24,60 @@ class SAPipeline(PiplineBase):
         self.len_pipeline: int = 10
         super().__init__(per_graph, arch_type, distance_table_bits, make_shuffle, self.len_pipeline, n_threads, )
 
-    # fixme numba tests
     def run(self, n_copies: int = 1) -> dict:
-        exec_times = 10
+        exec_times = 1000
         reports = {}
-        for exec_num in range(n_copies):
-            exec_key = 'exec_%d' % exec_num
+        # rets = []
+        with ProcessPoolExecutor(max_workers=8) as executor:
+            for exec_num in range(n_copies):
+                exec_key = 'exec_%d' % exec_num
+                max_counter = pow(self.per_graph.n_cells, 2) * exec_times
+                rets = executor.submit(self.exec_pipeline, max_counter=max_counter)
+                # exec_counter, n2c = self.exec_pipeline(max_counter)
 
-            n2c, c2n = self.init_sa_placement_tables()
+                # reports[exec_key] = Util.create_exec_report(self, exec_num, max_counter,
+                #                                            [exec_counter for _ in range(self.n_threads)], n2c)
 
-            st0 = Stage0SA(self.per_graph.n_cells, self.n_threads)
-            st1 = Stage1SA(c2n, self.n_threads)
-            st2 = Stage2SA(self.per_graph.neighbors)
-            st3 = Stage3SA(n2c, self.n_threads)
-            st4 = Stage4SA(self.arch_type, self.n_lines, self.n_columns)
-            st5 = Stage5SA(self.arch_type, self.n_lines, self.n_columns)
-            st6 = Stage6SA()
-            st7 = Stage7SA()
-            st8 = Stage8SA()
-            st9 = Stage9SA()
-            st10 = Stage10SA()
-
-            counter = 0
-            max_counter = pow(self.per_graph.n_cells, 2) * exec_times
-            pool = Pool()
-            while counter < max_counter:
-                pool.apply_async(st0.compute)
-                pool.apply_async(st1.compute, st0.old_output, st9.old_output, st1.old_output['wb']).wait()
-                pool.apply_async(st2.compute, st1.old_output).wait()
-                pool.apply_async(st3.compute, st2.old_output, st3.old_output['wb']).wait()
-                pool.apply_async(st4.compute, st3.old_output).wait()
-                pool.apply_async(st5.compute, st4.old_output).wait()
-                pool.apply_async(st6.compute, st5.old_output).wait()
-                pool.apply_async(st7.compute, st6.old_output ).wait()
-                pool.apply_async(st8.compute, st7.old_output ).wait()
-                pool.apply_async(st9.compute, st8.old_output ).wait()
-                pool.apply_async(st10.compute, st9.old_output,).wait()
-                '''st0.compute()
-                st1.compute(st0.old_output, st9.old_output, st1.old_output['wb'])
-                st2.compute(st1.old_output)
-                st3.compute(st2.old_output, st3.old_output['wb'])
-                st4.compute(st3.old_output)
-                st5.compute(st4.old_output)
-                st6.compute(st5.old_output)
-                st7.compute(st6.old_output)
-                st8.compute(st7.old_output)
-                st9.compute(st8.old_output)
-                st10.compute(st9.old_output)'''
-
-                counter += 1
-            for th_idx in range(self.n_threads):
-                for n_idx in range(len(n2c[th_idx])):
-                    if n2c[th_idx][n_idx] is not None:
-                        n2c[th_idx][n_idx] = Util.get_line_column_from_cell(n2c[th_idx][n_idx], self.n_lines,
-                                                                            self.n_columns)
-                    else:
-                        n2c[th_idx][n_idx] = [None, None]
-            reports[exec_key] = Util.create_exec_report(self, exec_num, counter,
-                                                        [st0.exec_counter for _ in range(self.n_threads)], n2c)
         report = Util.create_report(self, "SA_PIPELINE", n_copies, reports)
         return report
+
+    # fixme numba tests
+    def exec_pipeline(self, max_counter) -> list:
+
+        n2c, c2n = self.init_sa_placement_tables()
+
+        st0 = Stage0SA(self.per_graph.n_cells, self.n_threads)
+        st1 = Stage1SA(c2n, self.n_threads)
+        st2 = Stage2SA(self.per_graph.neighbors)
+        st3 = Stage3SA(n2c, self.n_threads)
+        st4 = Stage4SA(self.arch_type, self.n_lines, self.n_columns)
+        st5 = Stage5SA(self.arch_type, self.n_lines, self.n_columns)
+        st6 = Stage6SA()
+        st7 = Stage7SA()
+        st8 = Stage8SA()
+        st9 = Stage9SA()
+        st10 = Stage10SA()
+        counter = 0
+
+        while counter < max_counter:
+            st0.compute()
+            st1.compute(st0.old_output, st9.old_output, st1.old_output['wb'])
+            st2.compute(st1.old_output)
+            st3.compute(st2.old_output, st3.old_output['wb'])
+            st4.compute(st3.old_output)
+            st5.compute(st4.old_output)
+            st6.compute(st5.old_output)
+            st7.compute(st6.old_output)
+            st8.compute(st7.old_output)
+            st9.compute(st8.old_output)
+            st10.compute(st9.old_output)
+
+            counter += 1
+        for th_idx in range(self.n_threads):
+            for n_idx in range(len(n2c[th_idx])):
+                if n2c[th_idx][n_idx] is not None:
+                    n2c[th_idx][n_idx] = Util.get_line_column_from_cell(n2c[th_idx][n_idx], self.n_lines,
+                                                                        self.n_columns)
+                else:
+                    n2c[th_idx][n_idx] = [None, None]
+        return st0.exec_counter, n2c
