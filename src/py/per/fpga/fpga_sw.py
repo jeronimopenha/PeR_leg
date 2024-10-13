01b857cb253b1b57386fec89753d148a947a4923
Jeronimo Penha <jeronimopenha@gmail.com>
2024-10-12 21:26:09 -0300

    Working on histograms and reports

--- src/py/per/fpga/fpga_sw.py
+++ src/py/per/fpga/fpga_sw.py
@@ -1,13 +1,16 @@
+import json
 import random
 from src.py.graph.graph import Graph
 from src.py.per.base.per import PeR, EdgesAlgEnum
-from typing import List, Tuple
+from math import ceil, sqrt
+
+from src.py.util.util import Util
 
 
 class FPGAPeR(PeR):
     def __init__(self, graph: Graph):
         super().__init__()
-        random.seed(0)
+        #random.seed(0)
         self.graph = graph
 
     def per_sa(self):
@@ -16,6 +19,9 @@ class FPGAPeR(PeR):
     def per_yoto(self, n_exec: int = 1, edges_alg: EdgesAlgEnum = EdgesAlgEnum.ZIG_ZAG):
         # Final placements
         placements = []
+
+        # distances histogram
+        rel = {}
 
         # I will initialize the vector with the possible positions for inputs and outputs
         possible_pos_in, possible_pos_out = self.get_in_out_pos()
@@ -86,9 +92,89 @@ class FPGAPeR(PeR):
                     if placed:
                         break
 
-                a = 1
+            h, tc = self.calc_total_distance(n2c)
+            rel[exec] = {
+                'exec_id': exec,
+                'total_cost': tc,
+                'histogram': h,
+            }
         # TODO Fazer o relatório e os cálculos de distancias, gerar dot.
+        for r in rel.keys():
+            self.save_json(Util.get_project_root() + f"/benchmarks/fpga/",
+                           f"{self.graph.dot_name}_{rel[r]['exec_id']}.txt",
+                           rel[r])
         pass
+
+    def save_json(self, path: str, file_name: str, data):
+        if path[-1] != '/':
+            path = path + '/'
+        with open(path + file_name + '.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    def write_dot(self, placement, n2c):
+        output_dot_file = self.graph.dot_path + ".placed.dot"
+        dot_head = 'digraph layout{\n rankdir=TB;\n splines=ortho;\n node [style=filled shape=square fixedsize=true width=0.6];\n'
+        dot_foot = 'edge [constraint=true, style=invis];\n'
+
+        for i in range(self.graph.n_cells_sqrt):
+            for j in range(self.graph.n_cells_sqrt):
+                dot_foot = dot_foot + '%d' % (j * self.graph.n_cells_sqrt + i)
+                if (j + 1) % self.graph.n_cells_sqrt == 0:
+                    dot_foot = dot_foot + ';\n'
+                else:
+                    dot_foot = dot_foot + ' -> '
+
+        for i in range(self.graph.n_cells_sqrt):
+            if i % self.graph.n_cells_sqrt == 0:
+                dot_foot = dot_foot + 'rank = same {'
+            dot_foot = dot_foot + '%d' % i
+            if (i + 1) % self.graph.n_cells_sqrt == 0:
+                dot_foot = dot_foot + '};\n'
+            else:
+                dot_foot = dot_foot + ' -> '
+
+        dot_foot = dot_foot + '}'
+
+        str_out = dot_head
+
+        input_nodes = [self.graph.nodes_to_idx[node] for node in self.graph.input_nodes]
+        output_nodes = [self.graph.nodes_to_idx[node] for node in self.graph.output_nodes]
+
+        for i in range(self.graph.n_cells):
+            if placement[i] in input_nodes:
+                str_out += '%d[label="%d", fontsize=8, fillcolor="%s"];\n' % (
+                    i, placement[i], '#d9d9d9')  ##ffffff')  # if int(c_content, 16) == 0 else '#d9d9d9')
+            elif placement[i] in output_nodes:
+                str_out += '%d[label="%d", fontsize=8, fillcolor="%s"];\n' % (
+                    i, placement[i], '#d9d9ff')  ##ffffff')  # if int(c_content, 16) == 0 else '#d9d9d9')
+            else:
+                str_out += '%d[label="%d", fontsize=8, fillcolor="%s"];\n' % (
+                    i, placement[i], '#ffffff')
+
+        for ed in self.graph.get_edges_idx(self.graph.edges_str):
+            a = ed[0]
+            b = ed[1]
+            str_out += f"{n2c[a]} -> {n2c[b]};\n"
+        str_out += dot_foot
+
+        with open(output_dot_file, 'w') as f:
+            f.write(str_out)
+        f.close()
+
+    def calc_total_distance(self, n2c):
+        total_distance = self.graph.n_edges * -1
+        distances = {}
+        for e in self.graph.get_edges_idx(self.graph.edges_str):
+            ia = e[0] // self.graph.n_cells_sqrt
+            ja = e[0] % self.graph.n_cells_sqrt
+            ib = e[1] // self.graph.n_cells_sqrt
+            jb = e[1] % self.graph.n_cells_sqrt
+            dist = abs(ia - ib) + abs(ja - jb)
+            if dist not in distances.keys():
+                distances[dist] = 0
+            distances[dist] += 1
+            total_distance += dist
+        return dict(sorted(distances.items())), total_distance
 
     def choose_position(self, placement, node, choices):
         while True:
--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from src.py.per.fpga.fpga_sw import FPGAPeR
 from src.py.util.util import Util
 
 root_path = Util.get_project_root()
-g = Graph(root_path + "/benchmarks/fpga/xor5.dot", "teste.dot")
+g = Graph(root_path + "/benchmarks/fpga/xor5.dot", "xor5")
 per = FPGAPeR(g)
-res = per.per_yoto()
+res = per.per_yoto(10)
 
 a = 1
